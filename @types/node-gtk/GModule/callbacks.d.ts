625f1feba0656204446cb3f3e59e90f84f1ac7a0
nmanumr <nmanumr@gmail.com>
2019-03-26 11:08:58 +0500

    fixed GModule types

--- @types/node-gtk/GModule/callbacks.d.ts
+++ @types/node-gtk/GModule/callbacks.d.ts
@@ -3,9 +3,9 @@
   * @param module the GModule.Module corresponding to the module which has just been loaded
   * @returns None on success, or a string describing the initialization error 
   */
-export declare function moduleCheckInit(module: import('../GModule').Module): string;
+export declare type moduleCheckInit = (module: import('../GModule').Module) => string;
 /**
   * Specifies the type of the module function called when it is unloaded. If a module contains a function named g_module_unload() it is called automatically when the module is unloaded. It is passed the GModule.Module structure.
   * @param module the GModule.Module about to be unloaded 
   */
-export declare function moduleUnload(module: import('../GModule').Module): void;
+export declare type moduleUnload = (module: import('../GModule').Module) => void;
